c681683ef94014f928dfc6e3970bd4a57d37943f
Павел Федоренко <pavelfedorenko@MacBook-Air-Pavel.local>
2023-06-25 14:54:22 +0300

    dd

--- index.js
+++ index.js
@@ -1,105 +1,9 @@
-// const form = document.querySelector(".modal");
-// let count = 1;
-// const data = [];
-// const request = new XMLHttpRequest();
-// const url = "http://65.108.155.26:8081/otp/";
-// let obj;
-
-// const renderForm = () => {
-//   const div = document.createElement("div");
-//   div.classList.add("modal-input");
-//   const inputName = document.createElement("input");
-//   inputName.dataset.id = count;
-//   inputName.classList.add("input-name");
-//   inputName.type = "text";
-//   inputName.placeholder = "Name";
-//   div.append(inputName);
-//   const inputSecret = document.createElement("input");
-//   inputSecret.dataset.id = count;
-//   inputSecret.classList.add("input-secret");
-//   inputSecret.type = "text";
-//   inputSecret.placeholder = "OTP Secret";
-//   div.append(inputSecret);
-//   const code = document.createElement("div");
-//   code.classList.add("code");
-//   div.append(code);
-//   const btn = document.createElement("button");
-//   btn.classList.add("btn-add");
-//   btn.type = "button";
-//   btn.innerText = "+";
-//   div.append(btn);
-//   form.appendChild(div);
-//   btn.addEventListener("click", () => {
-//     count += 1;
-//     renderForm();
-//     btn.classList.add("hide");
-//     submitBtn.classList.add("hide");
-//   });
-//   const submitBtn = document.createElement("button");
-//   submitBtn.classList.add("modal-btn");
-//   submitBtn.type = "submit";
-//   submitBtn.innerText = "Save";
-//   submitBtn.addEventListener("click", (event) => {
-//     event.preventDefault();
-//     const inputs = document.querySelectorAll(".modal-input");
-//     const otps = [];
-//     inputs.forEach((input) => {
-//       const name = input.querySelector(".input-name").value.trim();
-//       const secret = input.querySelector(".input-secret").value.trim();
-//       const id = input.querySelector(".input-name").dataset.id;
-//       if (name !== "" && secret !== "") {
-//         const otp = new OTP(name, secret, id);
-//         otps.push(otp);
-//       }
-//     });
-//     data.push(...otps);
-//     data.forEach(async (item) => {
-//       await fetch(`${url}${item.name}:${item.secret}`)
-//         .then((response) => {
-//           if (response.ok) {
-//             return response.text();
-//           } else {
-//             throw new Error("Request failed!");
-//           }
-//         })
-//         .then((responseData) => {
-//           console.log("Response:+", responseData);
-//           obj = JSON.parse(responseData);
-//         })
-//         .catch((error) => {
-//           console.log("Request error:", error);
-//         });
-//       const code = document.querySelectorAll(".code");
-
-//       code.forEach((codeItem) => {
-//         if (obj) {
-//           const obj1 = Object.values(obj);
-//           obj1.forEach((obj1Item) => {
-//             console.log(obj1Item.code);
-//             codeItem.innerText = obj1Item.code;
-//           });
-//         }
-//       });
-//     });
-//   });
-//   form.appendChild(submitBtn);
-// };
-
-// class OTP {
-//   constructor(name, secret, id) {
-//     this.name = name;
-//     this.secret = secret;
-//     this.id = id;
-//   }
-// }
-// renderForm();
-// const text = "<name1>:<otp secret1>,<name2>:<otp secret2>...";
 const form = document.querySelector(".modal");
 let count = 1;
-const data = [];
-const request = new XMLHttpRequest();
 const url = "http://65.108.155.26:8081/otp/";
-let obj;
+let data = [];
+let code = [];
+let timers = []; // Массив для хранения таймеров
 
 const renderForm = () => {
   const div = document.createElement("div");
@@ -116,9 +20,13 @@ const renderForm = () => {
   inputSecret.type = "text";
   inputSecret.placeholder = "OTP Secret";
   div.append(inputSecret);
-  const code = document.createElement("div");
-  code.classList.add("code");
-  div.append(code);
+  const codeElement = document.createElement("div");
+  codeElement.classList.add("code");
+  codeElement.dataset.id = count;
+  div.append(codeElement);
+  const indicator = document.createElement("div");
+  indicator.classList.add("indicator");
+  div.append(indicator);
   const btn = document.createElement("button");
   btn.classList.add("btn-add");
   btn.type = "button";
@@ -128,55 +36,59 @@ const renderForm = () => {
   btn.addEventListener("click", () => {
     count += 1;
     renderForm();
+    updateCodes();
     btn.classList.add("hide");
     submitBtn.classList.add("hide");
   });
   const submitBtn = document.createElement("button");
   submitBtn.classList.add("modal-btn");
-  submitBtn.type = "submit";
+  submitBtn.type = "button";
   submitBtn.innerText = "Save";
-  submitBtn.addEventListener("click", async (event) => {
-    event.preventDefault();
-    const inputs = document.querySelectorAll(".modal-input");
-    const otps = [];
-    inputs.forEach((input) => {
-      const name = input.querySelector(".input-name").value.trim();
-      const secret = input.querySelector(".input-secret").value.trim();
-      const id = input.querySelector(".input-name").dataset.id;
-      if (name !== "" && secret !== "") {
-        const otp = new OTP(name, secret, id);
-        otps.push(otp);
-      }
-    });
-    data.push(...otps);
-    for (const item of data) {
-      try {
-        const response = await fetch(`${url}${item.name}:${item.secret}`);
-        if (response.ok) {
-          const responseData = await response.text();
-          console.log("Response: ", responseData);
-          obj = JSON.parse(responseData);
-        } else {
-          throw new Error("Request failed!");
-        }
-      } catch (error) {
-        console.log("Request error:", error);
-      }
-      const codeItems = document.querySelectorAll(".code");
-      codeItems.forEach((codeItem, i) => {
-        if (obj) {
-          const objValues = Object.values(obj);
-          objValues.forEach((obj1Item) => {
-            if (obj1Item && obj1Item.code) {
-              console.log(obj1Item.code);
-              codeItem.innerText = obj1Item.code;
-            }
-          });
-        }
-      });
-    }
-  });
   form.appendChild(submitBtn);
+  submitBtn.addEventListener("click", () => {
+    saveData();
+    request();
+    console.log(data);
+  });
+};
+
+const saveData = () => {
+  data = [];
+  const inputsName = document.querySelectorAll(".input-name");
+  const inputsSecret = document.querySelectorAll(".input-secret");
+  for (let i = 0; i < inputsName.length; i++) {
+    const inputName = inputsName[i].value;
+    const inputSecret = inputsSecret[i].value;
+    const inputid = inputsName[i].dataset.id;
+    data.push(new OTP(inputName, inputSecret, inputid));
+  }
+};
+
+const request = async () => {
+  code = [];
+  for (const item of data) {
+    try {
+      const response = await fetch(`${url}${item.name}:${item.secret}`);
+      const responseData = await response.json();
+      const objvalue = Object.values(responseData);
+      code.push(objvalue[0].code);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  renderCode();
+};
+
+const renderCode = () => {
+  const codesDisplay = document.querySelectorAll(".code");
+  for (let i = 0; i < codesDisplay.length; i++) {
+    if (code[i] === undefined) {
+      codesDisplay[i].innerText = "Ошибка";
+    } else {
+      codesDisplay[i].innerText = code[i];
+      console.log(code);
+    }
+  }
 };
 
 class OTP {
@@ -186,4 +98,35 @@ class OTP {
     this.id = id;
   }
 }
+
+const createTimer = (indicator) => {
+  let secondsLeft = 30;
+  return setInterval(async () => {
+    secondsLeft--;
+    indicator.innerText = secondsLeft;
+
+    if (secondsLeft === 0) {
+      await request();
+      secondsLeft = 30;
+    }
+  }, 1000);
+};
+
+const updateCodes = () => {
+  const indicators = document.querySelectorAll(".indicator");
+
+  // Остановка предыдущих таймеров
+  timers.forEach((timer) => clearInterval(timer));
+  timers.length = 0;
+
+  // Создание новых таймеров для каждой формы
+  indicators.forEach((indicator) => {
+    const timer = createTimer(indicator);
+    timers.push(timer);
+  });
+
+  request();
+};
+
 renderForm();
+updateCodes();
